e1a7cb8241cb73be524b2a864faa3b9a4b425b48
Hridaya Prajapati <hridaya@pixelos.net>
2025-02-09 13:37:36 +0100

    config: Move fonts Makefile to it's own folder
    Change-Id: I73843829734c14e6a8bd503b4c7b321db1edbec7

--- fonts/fonts.mk
+++ fonts/fonts.mk
@@ -1,4 +1,6 @@
 # Fonts
- PRODUCT_COPY_FILES += \
-     vendor/minus/fonts/HarmonyOS-Sans-Italic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/HarmonyOS-Sans-Italic.ttf \
-     vendor/minus/fonts/HarmonyOS-Sans.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/HarmonyOS-Sans.ttf
+PRODUCT_COPY_FILES += \
+    vendor/minus/fonts/GoogleSans-Regular.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Regular.ttf \
+    vendor/minus/fonts/GoogleSans-Italic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Italic.ttf \
+    vendor/minus/fonts/GoogleSansClock-Regular.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSansClock-Regular.ttf \
+    vendor/minus/fonts/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml
