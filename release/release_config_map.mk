398a0bd141ffa3c85b3fbc76b6db2322aa7c82e5
SkyMinus <minus_rav@qq.com>
2025-03-15 20:23:04 +0800

    Bump to BP1A

--- release/release_config_map.mk
+++
@@ -1,9 +0,0 @@
-default_containers: "product"
-default_containers: "system"
-default_containers: "system_ext"
-default_containers: "vendor"
-
-release_config {
-    name: "ap4a"
-    flag_value_files: "build_config/ap4a.textproto"
-}
