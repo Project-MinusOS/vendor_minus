fa17957fe528d16f3119221402e99edb94e4621b
SkyMinus <minus_rav@qq.com>
2025-03-16 22:31:37 +0800

    vendor: fonts: Add HarmonyOS Sans font and change output dir

---
+++ fonts/fonts.mk
@@ -0,0 +1,4 @@
+# Fonts
+ PRODUCT_COPY_FILES += \
+     vendor/minus/fonts/HarmonyOS-Sans-Italic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/HarmonyOS-Sans-Italic.ttf \
+     vendor/minus/fonts/HarmonyOS-Sans.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/HarmonyOS-Sans.ttf
