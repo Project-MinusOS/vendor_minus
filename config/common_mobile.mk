5a3d5c2c36709a85076cee3a3e22f7e28affe676
SkyMinus <minus_rav@qq.com>
2025-03-16 16:42:46 +0800

    Drop old audio prebuilts

--- config/common_mobile.mk
+++ config/common_mobile.mk
@@ -1,17 +1,5 @@
-# Inherit common mobile Lineage stuff
+# Inherit common mobile Minus stuff
 $(call inherit-product, vendor/minus/config/common.mk)
-
-# Include AOSP audio files
-$(call inherit-product-if-exists, frameworks/base/data/sounds/AudioPackage14.mk)
-include vendor/minus/config/aosp_audio.mk
-
-# Include Lineage audio files
-include vendor/minus/config/minus_audio.mk
-
-# Default notification/alarm sounds
-PRODUCT_PRODUCT_PROPERTIES += \
-    ro.config.notification_sound=Argon.ogg \
-    ro.config.alarm_alert=Hassium.ogg
 
 # Media
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
