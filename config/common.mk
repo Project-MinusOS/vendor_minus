8fd155fbbcac05ce720d5ee77ea7c6fb6b5405f3
SkyMinus <minus_rav@qq.com>
2025-09-22 15:06:34 +0800

    config: Bring back adb_root
    Signed-off-by: SkyMinus <minus_rav@qq.com>

--- config/common.mk
+++ config/common.mk
@@ -160,6 +160,19 @@ PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
     system/bin/procmem
 endif
 
+# Root
+PRODUCT_PACKAGES += \
+    adb_root
+ifneq ($(TARGET_BUILD_VARIANT),user)
+ifeq ($(WITH_SU),true)
+PRODUCT_PACKAGES += \
+    su
+
+PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
+    system/xbin/su
+endif
+endif
+
 # SystemUI
 PRODUCT_DEXPREOPT_SPEED_APPS += \
     Launcher3QuickStep \
